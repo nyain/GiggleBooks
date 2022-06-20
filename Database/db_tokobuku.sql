08aed18e7b6d27db73eefaeb20612b30d8b589f3
iggafebrianv <iggafebrian4@gmail.com>
2022-06-20 20:07:51 +0700

    database

--- Database/db_tokobuku.sql
+++ Database/db_tokobuku.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2022 at 08:23 AM
+-- Generation Time: Jun 20, 2022 at 03:05 PM
 -- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.0
+-- PHP Version: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,6 +41,15 @@ CREATE TABLE `buku` (
   `deleted` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `judul_buku`, `id_pengarang`, `id_penerbit`, `id_kategori`, `stok`, `berat`, `harga`, `deskripsi`, `gambar`, `deleted`) VALUES
+('BU-1', 'L art de la Simplicite : How to Live More With Less', 'PG-2', 'PN-2', 'K-2', 14, 1, 150000, 'Product details\r\nFormat : Paperback | 256 pages\r\nDimensions : 128 x 176 x 26mm | 204g\r\nPublication date : 12 Jan 2017\r\nPublisher : Orion Publishing Co\r\nImprint : TRAPEZE\r\nPublication City/Country : London, United Kingdom\r\nLanguage : English\r\nISBN ; 9781409163862', 'lart.jpg', 0),
+('BU-2', 'Tuesday with Morrie', 'PG-3', 'PN-3', 'K-2', 3, 1, 125000, 'Maybe it was a grandparent, or a teacher, or a colleague.  Someone older, patient and wise, who understood you when you were young and searching, helped you see the world as a more profound place, gave you sound advice to help you make your way through it. For Mitch Albom, that person was Morrie Schwartz, his college professor from nearly twenty years ago.', 'morrie.jpg', 0),
+('BU-3', 'Harry Potter And The Philosopher Stone', 'PG-1', 'PN-1', 'K-1', -1, 1, 175000, 'Harry Potter has never even heard of Hogwarts when the letters start dropping on the doormat at number four, Privet Drive. Addressed in green ink on yellowish parchment with a purple seal, they are swiftly confiscated by his grisly aunt and uncle. Then, on Harry eleventh birthday, a great beetle eyed giant of a man called Rubeus Hagrid bursts in with some astonishing news: Harry Potter is a wizard, and he has a place at Hogwarts School of Witchcraft and Wizardry. An incredible adventure is about to begin These new editions of the classic and internationally bestselling, multi award winning series feature instantly pickupable new jackets by Jonny Duddle, with huge child appeal, to bring Harry Potter to the next generation of readers. It time to PASS THE MAGIC ON ... ', 'harry.jpg', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -57,6 +66,17 @@ CREATE TABLE `customer` (
   `deleted` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id_customer`, `nama_customer`, `jk_customer`, `alamat_customer`, `email_customer`, `telp_customer`, `deleted`) VALUES
+('CUS-1', 'Pria Aditya', 'Laki-Laki', 'Indonesia', 'adit@gmail.com', '081214237034', 0),
+('CUS-2', 'Vivi Baskara', 'Perempuan', 'Indonesia', 'vivi@gmail.com', '081112233344', 0),
+('CUS-3', 'Sari Wangi', 'Perempuan', 'Indonesia', 'sariwangi32@gmail', '081810180018', 1),
+('CUS-4', 'Paulino Oskar', 'Laki-Laki', 'Indonesia', 'oskarr@gmail.com', '089414483457', 0),
+('CUS-5', 'Stefanus Pangestu', 'Laki-Laki', 'US', 'stefpang@gmail.com', '89319046615', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -68,6 +88,14 @@ CREATE TABLE `kategori` (
   `nama_kategori` varchar(50) NOT NULL,
   `deleted` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `deleted`) VALUES
+('K-1', 'Fiction', 0),
+('K-2', 'Self help', 0);
 
 -- --------------------------------------------------------
 
@@ -84,6 +112,17 @@ CREATE TABLE `penerbit` (
   `deleted` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `penerbit`
+--
+
+INSERT INTO `penerbit` (`id_penerbit`, `nama_penerbit`, `alamat_penerbit`, `email_penerbit`, `telp_penerbit`, `deleted`) VALUES
+('PN-1', 'Bloomsbury Publishing PLC', 'UK', 'bpplc@gmail.com', '8265945621', 0),
+('PN-2', 'Orion', 'UK', 'orionpub@gmail.com', '5512082047', 0),
+('PN-3', 'Bantam Doubleday Dell Publishing Group Inc', 'US', 'bddpgi@gmail.com', '9414483457', 0),
+('PN-4', 'Penguin Books Ltd ', 'UK', 'penguinbuuk@gmail.com', '6677664233', 0),
+('PN-5', 'Harper Collins', 'US', 'harcol@gmail.com', '9319046615', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -99,6 +138,17 @@ CREATE TABLE `pengarang` (
   `deleted` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `pengarang`
+--
+
+INSERT INTO `pengarang` (`id_pengarang`, `nama_pengarang`, `alamat_pengarang`, `email_pengarang`, `telp_pengarang`, `deleted`) VALUES
+('PG-1', 'J.K Rowling', 'UK', 'jkrowling@gmail.com', '9255356889', 0),
+('PG-2', ' Dominique Loreau', 'France', 'dominique@gmail.com', '9455771107', 0),
+('PG-3', 'Mitch Albom', 'US', 'mitchie@gmail.com', '8848211093', 0),
+('PG-4', 'Haemin Sunim', 'South Korea', 'haesunim@gmail.com', '6857322082', 0),
+('PG-5', 'Paulo Coelho', 'Spain', 'coelho@gmail.com', '9922114900', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -113,6 +163,14 @@ CREATE TABLE `transaksi` (
   `jumlah` int(11) NOT NULL,
   `total` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_customer`, `id_buku`, `tgl_transaksi`, `jumlah`, `total`) VALUES
+('TR-1', 'CUS-2', 'BU-2', '2022-06-11 15:39:15', 1, 125000),
+('TR-2', 'CUS-2', 'BU-3', '2022-06-11 15:39:15', 2, 350000);
 
 --
 -- Triggers `transaksi`
@@ -137,6 +195,18 @@ CREATE TABLE `user` (
   `id_customer` varchar(20) DEFAULT NULL,
   `deleted` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`username`, `password`, `tipe_user`, `id_customer`, `deleted`) VALUES
+('admin', 'admin', 'Admin', NULL, 0),
+('adityapriaa', 'iniAdit', 'Customer', 'CUS-1', 0),
+('sitikusmini', 'misvivi', 'Customer', 'CUS-2', 0),
+('bukanteh', 'iniWangi', 'Customer', 'CUS-3', 1),
+('bukansijody', 'gatauah', 'Customer', 'CUS-4', 0),
+('stefphang', 'sipaling', 'Customer', 'CUS-5', 0);
 
 --
 -- Indexes for dumped tables
